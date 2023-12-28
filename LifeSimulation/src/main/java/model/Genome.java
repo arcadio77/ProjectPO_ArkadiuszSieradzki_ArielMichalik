fbe6e4eaaab54ad26a2586bb5d6b40625ba57767
Ariel Michalik <arie.michalik@gmail.com>
2023-12-28 19:13:25 +0100

    genome castowanie

--- LifeSimulation/src/main/java/model/Genome.java
+++ LifeSimulation/src/main/java/model/Genome.java
@@ -24,14 +24,14 @@ public class Genome {
         boolean rand = new Random().nextBoolean();
 
         int combinedEnergy = parent1.getEnergy() + parent2.getEnergy();
-        int parent1Share = parent1.getEnergy() / combinedEnergy;
-        int parent2Share = parent2.getEnergy() / combinedEnergy;
+        double parent1Share = (double) (parent1.getEnergy() / combinedEnergy); //castowanie na double gdzies musi być moze do naprawy?
+        double parent2Share = (double) (parent2.getEnergy() / combinedEnergy);
 
         if(rand){
-            int howManyIdxParent1 = parent1Share * parent1.getGenome().size();
+            int howManyIdxParent1 = (int)(parent1Share * parent1.getGenome().size());
             ArrayList<Integer> partOfParent1 = (ArrayList<Integer>) parent1.getGenome().subList(0, howManyIdxParent1);
 
-            int howManyIdxParent2 = parent2Share * parent2.getGenome().size();
+            int howManyIdxParent2 = (int)(parent2Share * parent2.getGenome().size());
             int startingIdx = parent2.getGenome().size() - howManyIdxParent2 - 1;
             ArrayList<Integer> partOfParent2 = (ArrayList<Integer>) parent2.getGenome().subList(startingIdx, parent2.getGenome().size());
 
@@ -40,11 +40,11 @@ public class Genome {
             this.genome = partOfParent1;
         }
         else{
-            int howManyIdxParent1 = parent1Share * parent1.getGenome().size();
+            int howManyIdxParent1 = (int)(parent1Share * parent1.getGenome().size());
             int startingIdx = parent1.getGenome().size() - howManyIdxParent1 - 1;
             ArrayList<Integer> partOfParent1 = (ArrayList<Integer>) parent1.getGenome().subList(startingIdx, parent1.getGenome().size());
 
-            int howManyIdxParent2 = parent2Share * parent2.getGenome().size();
+            int howManyIdxParent2 = (int)(parent2Share * parent2.getGenome().size());
             ArrayList<Integer> partOfParent2 = (ArrayList<Integer>) parent2.getGenome().subList(0, howManyIdxParent2);
 
             partOfParent1.addAll(partOfParent2); //combined
