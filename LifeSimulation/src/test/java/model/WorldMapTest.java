fcf239193bf81255f1b77180a156c718b1a25ff0
Ariel Michalik <arie.michalik@gmail.com>
2024-01-24 10:24:50 +0100

    joungleTests

--- LifeSimulation/src/test/java/model/WorldMapTest.java
+++ LifeSimulation/src/test/java/model/WorldMapTest.java
@@ -10,22 +10,22 @@ public class WorldMapTest {
         WorldMap map1 = new WorldMap(4, 5);
         map1.setJungleBounds();
         System.out.println(map1.setJungleBounds());
-        assertEquals(new Boundary( new Vector2d(0, 2), new Vector2d(4, 3)), map1.setJungleBounds());
+        assertEquals(new Boundary( new Vector2d(0, 2), new Vector2d(3, 3)), map1.setJungleBounds());
 
         WorldMap map2 = new WorldMap(4, 4);
         map2.setJungleBounds();
         System.out.println(map2.setJungleBounds());
-        assertEquals(new Boundary( new Vector2d(0, 2), new Vector2d(4, 3)), map2.setJungleBounds());
+        assertEquals(new Boundary( new Vector2d(0, 1), new Vector2d(3, 2)), map2.setJungleBounds());
 
         WorldMap map3 = new WorldMap(5, 4);
         map3.setJungleBounds();
         System.out.println(map3.setJungleBounds());
-        assertEquals(new Boundary( new Vector2d(0, 2), new Vector2d(5, 3)), map3.setJungleBounds());
+        assertEquals(new Boundary( new Vector2d(0, 1), new Vector2d(4, 2)), map3.setJungleBounds());
 
         WorldMap map4 = new WorldMap(10, 10);
         map4.setJungleBounds();
         System.out.println(map4.setJungleBounds());
-        assertEquals(new Boundary( new Vector2d(0, 4), new Vector2d(10, 6)), map3.setJungleBounds());
+        assertEquals(new Boundary( new Vector2d(0, 4), new Vector2d(9, 5)), map4.setJungleBounds());
 
     }
 
